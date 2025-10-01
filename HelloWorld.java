8037cfd069c0c4a9d1a16fe7cef53975f8e58674
Jatin Sharma <jatinsharm0312@gmail.com>
2025-10-01 16:34:23 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,5 @@
-public class Helloworld (
-  // This is the main method. It is the entry point of any Java application.
-  public static void main(String[] args) (
-  System.out.println("Hello, Java!");
-
-      // Call a method to add two numbers\
-      int result adumbers (10, 20);
-      System.out.println("The sum is: result);
-)
-
-        // User-defined method to add two integers
-        public static int addNumbers(int a, int b) ( 
-          return a+b;
- )
-)
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
